46cc0823b6848095e49bd1f43bce3866e41cd0a2
Andres Meira <andresmeira@gmail.com>
2020-11-24 14:45:28 -0600

    change ETH Power Pin > GPIO2

--- firmware/WatsonIoT/src/main.cpp
+++ firmware/WatsonIoT/src/main.cpp
@@ -46,7 +46,7 @@ PubSubClient mqtt(MQTT_HOST, MQTT_PORT, callback, wifiClient);
 #endif
 #define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
 // Pin# of the enable signal for the external crystal oscillator (-1 to disable for internal APLL source)
-#define ETH_POWER_PIN   -1
+#define ETH_POWER_PIN   2
 // Type of the Ethernet PHY (LAN8720 or TLK110)
 #define ETH_TYPE        ETH_PHY_LAN8720
 // I²C-address of Ethernet PHY (0 or 1 for LAN8720, 31 for TLK110)
